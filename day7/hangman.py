3b508f008a5e4bdcd4e9baa9da3bc0f2da5b4f29
nowon9159 <nowon9159@clouflake.com>
2023-04-17 11:24:12 +0900

    행맨 로직 생각, 단어의 범위 만큼 배열 담기

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,1 +1,22 @@
 #로직 생각해보기
+# 단어를 무작위 추출
+# 단어의 범위 만큼 배열에 담기
+# 인풋 값을 받아서 알파벳 비교, 배열의 인덱스 값에 따라 알파벳 비교
+# 알파벳이 있다면 프린트 없다면 목숨-1
+# 목숨 안에 단어를 맞추면 성공 -> 모든 빈칸이 채워졌는가?
+# 목숨 안에 단어를 못 맞추면 실패 -> 목숨이 0일때 모든 빈칸이 채워지지 않았는가?
+
+import random
+
+# 단어 배열에서 아무거나 추출
+words = ["book", "phone", "chair", "table", "cup", "pen", "pencil", "bag", "door", "key"]
+ran_word = random.choice(words)
+
+# 단어의 범위 만큼 다시 배열에 담기
+selected_word = []
+for i in range(0,len(ran_word)):
+    selected_word.append(ran_word[i])
+
+# 인풋 값 받기
+
+
