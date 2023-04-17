0860ce250d978dfd153386f38049251c0b099c3a
nowon9159 <nowon9159@clouflake.com>
2023-04-17 15:32:51 +0900

    hangman 완성, 8일차 추가

--- day7/hangman.py
+++ day7/hangman.py
@@ -4,28 +4,38 @@
 # 인풋 값을 받아서 알파벳 비교, 배열의 인덱스 값에 따라 알파벳 비교
 # 알파벳이 있다면 프린트 없다면 목숨-1
 # 목숨 안에 단어를 맞추면 성공 -> 모든 빈칸이 채워졌는가?
+# 만약 모든 빈칸이 채워지지 않고 목숨이 0 초과라면 계속 input
 # 목숨 안에 단어를 못 맞추면 실패 -> 목숨이 0일때 모든 빈칸이 채워지지 않았는가?
 
 import random
 
 # 단어 배열에서 아무거나 추출
-words = ["book", "phone", "chair", "table", "cup", "pen", "pencil", "bag", "door", "key"]
+words = ["book", "phone", "chair", "table", "cup", "pen", "pencil", "bag", "door", "key", "spoon"]
 ran_word = random.choice(words)
+len_word = len(ran_word)
 
-# 단어의 범위 만큼 다시 배열에 담기
-selected_word = []
-for i in range(0,len(ran_word)):
-    selected_word.append(ran_word[i])
+mark_word = []
+# 단어의 범위 만큼 언더 바 추가
+for i in range(len_word):
+    mark_word += "_"
 
-# 인풋 값 받기
 print(ran_word)
-guess_word = input("Guess a letter : ")
 
-# 유저 인풋 값 확인해서 선택된 배열의 값과 비교
-for i in range(0,len(ran_word)):
-    if guess_word == selected_word[i] :
-        print("Right")
-    else:
-        print("Wrong")
+life = len_word
 
+end_of_game = False
 
+while not end_of_game:
+    # 인풋 값 받기
+    input_word = input("Guess a letter: ").lower()
+
+    # 단어 배열이랑 인풋 값이랑 비교해서 일치하면 _ 배열에 insert
+    for i in range(len_word):
+        if ran_word[i] == input_word:
+            mark_word[i] = input_word
+    
+    print(mark_word)
+
+    if mark_word.count("_") == 0 :
+        end_of_game = True
+        print("You Win")
