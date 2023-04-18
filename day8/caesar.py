a6d4d068fc3d495003bc830c5880c42592fb10d1
nowon9159 <nowon9159@clouflake.com>
2023-04-18 14:34:24 +0900

    복호화 추가

--- day8/caesar.py
+++ day8/caesar.py
@@ -1,5 +1,6 @@
 alphabet = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
@@ -18,6 +19,7 @@ arr_encrypted_text = []
 encrypted_text = ""
 length_text = len(text)
 
+
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
 def encrypt(text, shift):
     arr_text = list(text)
@@ -29,9 +31,19 @@ def encrypt(text, shift):
         arr_encrypted_text.append(encrypted_text)
     # print(arr_encrypted_text)
     for i in range(len(arr_encrypted_text)):
-        encrypted_text = ''.join(arr_encrypted_text)
-        print("This is a encrypted input : " + encrypted_text)
+        encrypted_text = ''.join(arr_encrypted_text) # join 함수 => '구분자'.join(리스트)
+    print(f"This is a encrypted input : {encrypted_text}")
         
+def decrypt(text, shift):
+    cipher_text = ""
+    for i in text:
+        position = alphabet.index(i) # index 함수는 배열의 인덱스 값을 추출 가능
+        new_position = position - shift # shift 만큼 추출한 인덱스 값 빼기
+        new_letter = alphabet[new_position] # 뉴포지션의 인덱스 값을 기반으로 알파벳 값 뽑아 내 new_letter 집어 넣기
+        cipher_text += new_letter # cipher_text 에 new_letter 순차적으로 집어 넣기
+    print(f"The Decoded text is {cipher_text}")
+
+
 
 
 
@@ -52,3 +64,5 @@ def encrypt(text, shift):
 
 if direction == "encode":
     encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text, shift)
