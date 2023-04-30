402dd316b41fa059fdbe04284403df0fb265f5d9
nowon9159 <nowon9159@clouflake.com>
2023-04-30 16:22:37 +0900

    버그 해결, 로직 완성

--- day10/calculator.py
+++ day10/calculator.py
@@ -3,9 +3,11 @@
 # 숫자와 연산하기위한 또 다른 숫자를 선택함
 # 수식과 답을 표현 하고
 # y를 타이핑 하면 계산된 답으로 다시 계산할지 n을 타이핑하면 새로운 계산을 할지 선택
+# y를 타이핑 한 두번째 연산 부터는 first_number의 인풋 값을 받지 않고 연산 된 값을 first_number로 대체
 
 value_continue = True
-after_one_operation = False
+value_after_2ndoperation = "False"
+operation_number = 0
 
 def calculator(first_number, next_number, value_operation):
     if value_operation == "+":
@@ -17,19 +19,24 @@ def calculator(first_number, next_number, value_operation):
     elif value_operation == "/":
         return first_number / next_number
     else :
-        print("You Have a Wrong Input.")
-        value_continue = False
+        print("You Put a Wrong Input.")
 
 while value_continue :
-    first_number = int(input("What's the first number?: ")) # 첫째 값 받는다
+    if value_after_2ndoperation == "True" :
+        first_number = operation_number
+    elif value_after_2ndoperation == "False" :
+        first_number = int(input("What's the first number?: ")) # 첫째 값 받는다
+
     value_operation = input("+\n-\n*\n/\nPick an operation: ") # 어떤 연산을 할 지 고르기
     next_number = int(input("What's the next number?: ")) # 둘째 값 받는다
-
+    
     operation_number = calculator(first_number, next_number, value_operation)
 
-    check_continue = input(f"Type 'y' to continue calculating with {operation_number} or type 'n' to start a new claculation: ") # 연산을 계속 할 지 결정
+    check_continue = input(f"Type 'y' to continue calculating with {operation_number} or type 'n' to start a new claculation: ").lower() # 연산을 계속 할 지 결정
 
-    if check_continue == "y":
-        first_number = operation_number
-    elif check_continue == "n":
+    if check_continue == "n":
+        value_continue = False
         break
+    elif check_continue == "y":
+        value_continue = True
+        value_after_2ndoperation = "True"
