74bd83b93f3f6f8b27f22d4feb796d13d89cc2e3
ddubi <ddubi691@naver.com>
2023-05-05 21:26:57 +0900

    게임 로직 작성 중

--- day11/blackjack.py
+++ day11/blackjack.py
@@ -16,6 +16,33 @@ import random as ran
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10]
 ran_card = ran.choice(cards)
 
+# 빈 배열 생성
 diller_score = []
 my_score = []
+continue_value = True
 
+# 빈 배열에 랜덤 값 넣기
+first_card_diller = diller_score.append(ran_card)
+second_card_diller = diller_score.append(ran_card)
+first_card_my = my_score.append(ran_card)
+second_card_my = my_score.append(ran_card)
+
+# 딜러와 내 스코어 합산
+sum_diller_score = sum(diller_score)
+sum_my_score = sum(my_score)
+
+# 딜러 카드 & 내 스코어 오픈
+print(f"Diller's second number is : {diller_score[1]}")
+print(f"Your score is {sum_my_score}")
+
+
+while continue_value : 
+    if sum_diller_score > 20 :
+        print("You win")
+        continue_value = False
+    elif sum_my_score > 20 :
+        print("You Lose")
+        continue_value = False
+    elif sum_diller_score == sum_my_score :
+        print("Draw")
+        continue_value = False
