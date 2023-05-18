062a51231d199203a4b26c303547e4dcf045ed4f
nowon9159 <nowon9159@clouflake.com>
2023-05-18 17:57:22 +0900

    coffee machine 로직 작성 중

--- day15/coffe_machine.py
+++ day15/coffe_machine.py
@@ -13,46 +13,55 @@
 # -> 사용자가 라떼를 주문했다고 가정했을 때 동전을 넣으면 거스름돈이 나오고 라떼도 나온다. 보고서를 확인하면 물이 100ml 밖에 없는 것 확인 가능
 # -> 이런 경우에서 물이 필요한데 채워 넣지 않고 주문을 하면 죄송합니다 문구 출력
 # 3. 동전을 처리할 수 있어야 한다.
-# -> 동전을 종류별로 넣어서 커피의 가격보다 낮은 가격의 동전을 넣게 되면 커피는 나오지 않고 동전은 반환 된다. # Sorry that's not enough money. Money refunded.
+# -> 동전을 종류별로 넣어서 커피의 가격보다 낮은 가격의 동전을 넣게 되면 커피는 나오지 않고 동전은 반환 된다.
 # -> 커피의 가격보다 높은 가격의 동전을 넣게 되면 커피 가격을 기준으로 거스름돈을 주게 된다.
 
-import data
+from data import menu
 
+# 현재 남은 리소스 선언
 resources = {
     "water": 300, # ml
     "milk": 200, # ml
     "coffee": 100, # g
 }
 
-cost = {
-    "cent": 1,
-    "dime": 10,
-    "nickel": 5,
-    "quarter": 25
-}
-
 today_take = 0
 
-def check_cost(quaters_input, dimes_input, nickels_input, cents_input) :
-    quaters_input
-    dimes_input
-    nickels_input
-    cents_input
-    return 
+
+## 커피 만드는 로직 재 정리
+# 1. 어떤 커피인지 알아야 함 -> 함수 선언 시 input 값 파라미터로 받아서 함수 안에서 체크
+# 2. 해당 커피를 만들 때 드는 리소스 비용을 현재 리소스 비용과 비교해야함.
+# -> 어떤 커피인지 체크 후에 리소스 얼마 남았는 지 비교
+# 2.1 비교해서 리소스 비용이 더 많으면 진행 적으면 return false
+def make_menu(user_input, resources):
+    dict_resources = resources
+    dict_ingre = menu[user_input]['ingredients']
+    if user_input == 'espresso':
+        
+    elif user_input == 'latte' or user_input == 'cappuccino' :
+
+    elif user_input == 'report':
 
 
+## 돈계산 로직 재 정리
+# 만약 
+
+def check_all_take(quaters_input, dimes_input, nickels_input, cents_input) :
+    all_cost = (quaters_input * 0.01) + (dimes_input * 0.1) + (nickels_input * 0.05) + (cents_input * 0.25)
+    
 
 
 while True :
-    
+    # 오늘 번 돈
     user_input = input("What would you like? (espresso/latte/cappuccino): ")
+    
     print("Please insert coins.")
     quaters_input = input("how many quarters?: ")
     dimes_input = input("how many dimes?: ")
     nickels_input = input("how many nickels?: ")
     cents_input = input("how many cents?: ")
 
-    check_cost(quaters_input, dimes_input, nickels_input, cents_input)
+    check_all_take(quaters_input, dimes_input, nickels_input, cents_input)
 
     print(f"Here is ${} in change.")
     print(f"Here is your espresso ☕. Enjoy!")
