bc3681a0a1ccccc44f60eaad3cd8580945cfde26
nowon9159 <nowon9159@clouflake.com>
2023-05-19 17:20:56 +0900

    feature / 100 걸음 걸어가게 하기

--- day16/main.py
+++ day16/main.py
@@ -8,9 +8,13 @@ from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+
 timmy.shape("turtle")
 # 과제 1. 거북이 색깔 바꾸기
 timmy.color("red")
+# 과제 2. 거북이 100 걸음 앞으로 가게 하기
+timmy.forward(100)
+
 
 my_screen = Screen()
 print(my_screen.canvheight)
