d289d03b551d3dd26a6817d4bc37da0fcca23029
nowon9159 <nowon9159@clouflake.com>
2023-05-23 17:54:02 +0900

    init main / challenge 1 complete

---
+++ day18/main.py
@@ -0,0 +1,27 @@
+## 터틀 모듈을 이용한 그래픽 작업
+
+
+## 로직
+#
+
+from turtle import Turtle, Screen
+
+tim = Turtle()
+tim.shape("circle")
+tim.color("red")
+
+
+# 과제 1
+# 1. turtle 이용해서 정사각형 그리기
+# 2. 한 변의 길이가 100, 화면 어느 지점에 그리던 상관 없음
+
+# for i in range(4):
+#   tim.forward(100)
+#   tim.left(90)
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
