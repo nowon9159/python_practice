d401c284932831ea5db2ce2dfb97e7b53cff1c7c
nowon9159 <nowon9159@clouflake.com>
2023-05-23 13:16:57 +0900

    challenge 1 Question 클래스 생성하기 (text와 answer을 속성으로 받는 메소드 생성)

---
+++ day17/question_model.py
@@ -0,0 +1,5 @@
+# Create a Question class with an __init__ method with two attributes: text and answer arttribute
+class Question:
+    def __init__(self, text, answer) :
+        self.text = text
+        self.answer = answer
