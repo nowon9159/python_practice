a40cf30e0158794cc37016776317cd89dd0f9e6c
nowon9159 <nowon9159@clouflake.com>
2023-05-23 16:52:13 +0900

    question model, quiz brain 원복

--- day17/question_model.py
+++ day17/question_model.py
@@ -1,5 +1,5 @@
 # Create a Question class with an __init__ method with two attributes: text and answer arttribute
 class Question:
     def __init__(self, q_text, q_answer):
-        self.question = q_text
-        self.correct_answer = q_answer
+        self.text = q_text
+        self.answer = q_answer
