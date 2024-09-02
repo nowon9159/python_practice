883226a10ec920c2ba1e317ab0fafcc66c64fae1
nowon9159 <nowon9159@gmail.com>
2024-09-02 10:00:48 +0900

    Revert "Init project"
    This reverts commit 29884da9d63fc5eb496811d564865689c1aa1ea2.

--- nowon9159_forum/projects/forum.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\Users\Administrator\Documents\python_practice\nowon9159_forum\projects
-C:\Users\Administrator\Documents\python_practice\nowon9159_forum\venvs\nowon9159_forum\Scripts\activate
