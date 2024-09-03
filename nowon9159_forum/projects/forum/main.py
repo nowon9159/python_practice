e7a464ef28df6f51d33cf554e51b2368385dea07
nowon9159 <nowon9159@gmail.com>
2024-09-03 17:23:10 +0900

    feat: Add question router

--- nowon9159_forum/projects/forum/main.py
+++ nowon9159_forum/projects/forum/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from starlette.middleware.cors import CORSMiddleware
 
+from domain.question import question_router
+
+
 app = FastAPI()
 
 origins = [
@@ -16,7 +19,4 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/hello")
-
-def hello():
-    return {"message": "안녕하세요"}
+app.include_router(question_router.router)
