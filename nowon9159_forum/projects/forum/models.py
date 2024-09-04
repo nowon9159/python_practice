63f9db35844ea4fdc9ebf3d57fac9f38c8a6f24c
nowon9159 <nowon9159@gmail.com>
2024-09-04 16:41:02 +0900

    Feat: Add dummy data to db

--- nowon9159_forum/projects/forum/models.py
+++ nowon9159_forum/projects/forum/models.py
@@ -7,7 +7,7 @@ from database import Base
 class Question(Base):
     __tablename__ = "question"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     subject = Column(String, nullable=False)
     content = Column(Text, nullable=False)
     create_date = Column(DateTime, nullable=False)
